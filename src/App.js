9b80848e89c57c208d53c08ffbd4d5e1f7ec64ee
kishu1998 <kishorsanjaypatil1998@gmail.com>
2019-08-06 17:07:08 +0530

    hosted

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo, { ReactComponent } from './logo.svg';
 import './App.css';
+class Hi extends React.Component
+{
+    constructor() {
+    super();
 
-function App() {
+    this.state = {person: []};
+}
+  componentDidMount() {
+    fetch('https://api.myjson.com/bins/t06ot')
+    .then(response =>  response.json())
+    .then(resData => {
+       //console.log(JSON.stringify(resData))
+       //do your logic here       
+       //let person = resData.results
+       this.setState({ person: resData.results }); //this is an asynchronous function
+    })
+}
+
+render(){
+  let l=[];
+ l=this.state.person.map((personRecord) => {
+  return <div key={personRecord.id.value}> {personRecord.name.first} </div>
+});
+  return(
+  <div>
+    { 
+      l
+    }
+  </div>
+  )
+}
+}
+class Showproducts extends React.Component
+{
+  
+  render()
+  {
+    
+    let lastcategory=null;
+    const list=[];
+    this.props.products.forEach((element,index) => {
+      if(!element.name.includes(this.props.tobeshown)) return;
+      if(this.props.check && !element.stocked) return;
+      
+      if(element.category!==lastcategory)
+    list.push(<li key={element.category} className="category" >{element.category}</li>)
+      let name=element.stocked?element.name:<span style={{color:'red'}} >{element.name}</span>
+      list.push(<li key={element.name} className="list" >{name} &nbsp;&nbsp; {element.price}</li>)
+    lastcategory=element.category;
+    });
+  
+    return <div><ul>
+      {list}
+      </ul></div>
+
+  }
+}
+class SearchBar extends React.Component
+{
+  constructor()
+  {
+    super()
+    this.change = this.change.bind(this);
+  } 
+  change(e)
+  {
+    this.props.OnTermChange(e.target.value);
+  }
+  handle()
+  {
+    this.props.tickmark();
+  }
+  render()
+  {
+    return <div><div><input onChange={event=>this.change(event)} /></div>
+    <p>
+      <input type='checkbox' onClick={()=>this.handle()} />
+      only show products in stock
+    </p>
+    </div>
+  }
+}
+class App extends React.Component {
+constructor()
+{
+  super()
+  this.state={
+    check:false,
+    searchval:''
+  }
+  this.OnTermChange = this.OnTermChange.bind(this);
+  this.tickmark = this.tickmark.bind(this);
+}
+OnTermChange(val)
+  {
+    
+    this.setState({searchval:val})
+  }
+tickmark()
+{
+  
+  this.setState({check:!this.state.check})
+
+}
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='container'>
+      <SearchBar OnTermChange={(i)=>this.OnTermChange(i)} tickmark={this.tickmark} ></SearchBar>
+      <Showproducts tobeshown={this.state.searchval} check={this.state.check} products={this.props.products}/> 
+      
     </div>
   );
+}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const products=[
+    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+  ];
+ReactDOM.render(<App products={products}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
